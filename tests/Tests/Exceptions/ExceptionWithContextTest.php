1059f16d3bd51805912685674da415a1fed65c55
Charles Berube <charles.berube@cascadeenergy.com>
2015-07-20 15:17:23 -0700

    Clean up some extra spacing

--- tests/Tests/Exceptions/ExceptionWithContextTest.php
+++ tests/Tests/Exceptions/ExceptionWithContextTest.php
@@ -9,7 +9,6 @@ class ExceptionWithContextTest extends \PHPUnit_Framework_TestCase
     public function testItShouldPassThroughStandardDataToTheBaseExceptionClass()
     {
         $previousException = new \Exception('previousMessage');
-
         $exception = new ExceptionWithContext('message', [], 42, $previousException);
 
         $this->assertEquals('message', $exception->getMessage());
@@ -20,7 +19,6 @@ class ExceptionWithContextTest extends \PHPUnit_Framework_TestCase
     public function testItShouldAllowTheContextToBeRetrieved()
     {
         $context = new \stdClass();
-
         $exception = new ExceptionWithContext('message', $context);
 
         $this->assertSame($context, $exception->getContext());
