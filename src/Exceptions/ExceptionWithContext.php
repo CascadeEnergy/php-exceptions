a90705f1b9f09dd9d0bae330fa119872eda5fc1e
Charles Berube <charles.berube@cascadeenergy.com>
2015-07-20 13:59:11 -0700

    Initial import; add unit tests

---
+++ src/Exceptions/ExceptionWithContext.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Cascade\Exceptions;
+
+/**
+ * A custom exception class which provides a context argument for later reference.
+ */
+class ExceptionWithContext extends \Exception
+{
+    /** @var mixed The context of the exception */
+    private $context;
+
+    /**
+     * @param string $message The message for the exception
+     * @param mixed|null $context The context of the exception (it is recommended that this be an object or hash)
+     * @param int $code The exception code
+     * @param \Exception|null $previous The previous exception, if any
+     */
+    public function __construct($message, $context = null, $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->context = $context;
+    }
+
+    /**
+     * Returns the context of the exception.
+     *
+     * @return mixed|null
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+}
---
+++ tests/Tests/Exceptions/ExceptionWithContextTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cascade\Tests\Exceptions;
+
+use Cascade\Exceptions\ExceptionWithContext;
+
+class ExceptionWithContextTest extends \PHPUnit_Framework_TestCase
+{
+    public function testItShouldPassThroughStandardDataToTheBaseExceptionClass()
+    {
+        $previousException = new \Exception('previousMessage');
+
+        $exception = new ExceptionWithContext('message', [], 42, $previousException);
+
+        $this->assertEquals('message', $exception->getMessage());
+        $this->assertEquals(42, $exception->getCode());
+        $this->assertSame($previousException, $exception->getPrevious());
+    }
+
+    public function testItShouldAllowTheContextToBeRetrieved()
+    {
+        $context = new \stdClass();
+
+        $exception = new ExceptionWithContext('message', $context);
+
+        $this->assertSame($context, $exception->getContext());
+    }
+}
